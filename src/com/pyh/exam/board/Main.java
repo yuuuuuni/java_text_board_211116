671d11a616d90579e13e08ee94d9881ccf8b2632
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-17 12:01:59 +0900

    매개변수있는 생성자를 추가하여, 게시물 객체를 쉽게 만들도록

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -36,10 +36,7 @@ public class Main {
 
 
 
-        Article article = new Article();
-        article.id = id;
-        article.title = title;
-        article.body = body;
+        Article article = new Article(id, title, body);
         System.out.println("생성된 게시물 객체 : " + article);
 
         System.out.println(article.id + "번 게시물이 등록되었습니다.");
@@ -63,9 +60,15 @@ class Article {
   String title;
   String body;
 
+  Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+
 
 @Override
 public String toString() {
-  return String.format("{ id : " + id + ", title : " + title + "}" );
+  return String.format("{ id : " + id + ", title : " + title + ", body : " + body + "}" );
  }
 }
