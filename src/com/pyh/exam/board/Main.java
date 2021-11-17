6c57431eca7dd557bea2e3dd36cb38b655ddb4b2
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-17 09:54:54 +0900

    새 게시물 입력받기 기능 구현 시작

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -1,5 +1,6 @@
 package com.pyh.exam.board;
 
+import java.net.StandardSocketOptions;
 import java.util.Scanner;
 
 public class Main {
@@ -7,27 +8,48 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    String str;
 
-    System.out.println("== 게시판 v 0.1 ==");
-    System.out.println("== 프로그램 시작 ==");
 
-    while(true) {
+    while (true) {
+
       System.out.printf("명령) ");
-      str = sc.nextLine();
+      String str = sc.nextLine();
 
       if(str.equals("exit")) {
         break;
       }
 
-      System.out.println("입력된 명령어 : " + str);
-
+      else if(str.equals("/usr/article/write")) {
+        System.out.println("- 게시물 등록 -");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = 1;
+        System.out.println(id + "번 게시물이 등록되었습니다.");
+      }
+      else {
+        System.out.printf("입력된 명령어 : " + str);
+      }
     }
 
 
-
-    System.out.println("== 프로그램 종료 ==");
-
-    sc.close();
+//    System.out.println("== 게시판 v 0.1 ==");
+//    System.out.println("== 프로그램 시작 ==");
+//
+//    while(true) {
+//      System.out.printf("명령) ");
+//      str = sc.nextLine();
+//
+//      if(str.equals("exit")) {
+//        break;
+//      }
+//
+//    }
+//
+//    System.out.println("입력된 명령어 : " + str);
+//    System.out.println("== 프로그램 종료 ==");
+//
+//    sc.close();
   }
 }
