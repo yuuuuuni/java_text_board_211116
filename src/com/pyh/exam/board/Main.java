3c98eae87847ed00cbb3c65ddad80d4c19d18e99
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-17 10:29:49 +0900

    입력받은 게시물 정보를 Article 객체에 저장

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -8,6 +8,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
@@ -30,9 +31,15 @@ public class Main {
         String body = sc.nextLine();
 
         id = articlesLastId + 1;
-        articlesLastId ++;
+        articlesLastId = id;
 
-        System.out.println(id + "번 게시물이 등록되었습니다.");
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+        System.out.println("생성된 게시물 객체 : " + article);
+
+        System.out.println(article.id + "번 게시물이 등록되었습니다.");
       }
       else {
         System.out.println("입력된 명령어 : " + str);
@@ -45,4 +52,12 @@ public class Main {
 
 
   }
+}
+
+
+class Article {
+  int id;
+  String title;
+  String body;
+
 }
