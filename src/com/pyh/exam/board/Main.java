3b8a6ab8fa1bab41ff0c8e0a5b5ae3dd158ab5de
yuuuuuni <yuuuuuni@gmail.com>
2021-11-22 09:12:36 +0900

    /usr/article/list 로 입력하면 게시물리스트 출력, 시작

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -1,6 +1,8 @@
 package com.pyh.exam.board;
 
+import java.lang.reflect.Array;
 import java.net.StandardSocketOptions;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +16,13 @@ public class Main {
 
     int articlesLastId = 0;
     Article lastArticle = null;  // 그냥 int i = 1; String a = "안녕" 처럼 그냥 저것도 변수타입 변수명 = 값이라고 생각하면 됨  Article이 변수타입, article이 변수명
+    ArrayList<Article> articles = new ArrayList<Article>();
+
+    // 테스트 데이터 3개 등록, 시작
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
+    // 테스트 데이터 3개 등록, 끝
 
     while (true) {
       System.out.printf("명령) ");
@@ -23,7 +32,18 @@ public class Main {
         System.out.println("입력된 명령어 : " + cmd);
         break;
       }
+      else if(cmd.equals("/usr/article/list")) {
 
+        System.out.println("- 게시물 리스트 -");
+        System.out.println("--------------------");
+        System.out.println("번호 / 제목");
+        System.out.println("--------------------");
+
+        for (Article article : articles) {
+          System.out.println(article.id + "/" + article.title);
+        }
+
+      }
       else if(cmd.equals("/usr/article/detail")) {
 
         if(lastArticle == null) {
@@ -53,7 +73,7 @@ public class Main {
 
 
         Article article = new Article(id, title, body);
-        lastArticle = article;
+        lastArticle = article;  // article에 있는 객체가 lastArticle로 옮겨지는게 아니라 얘네 둘은 그냥 리모콘임 리모콘이 두개가 생기는 것임
         System.out.println("생성된 게시물 객체 : " + article);
 
         System.out.println(article.id + "번 게시물이 등록되었습니다.");
